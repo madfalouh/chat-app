339756473ae53617fff50df9c9cc7676795f6f4f
s.elkharraz <saif.elkharraz@seven-app.net>
2024-11-12 16:55:49 +0100

    create front and back projects

---
+++ chat-back/src/test/java/com/example/chatback/ChatBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
