b12b0f6644e0a68e7ac2265998e316d91243ec06
s.elkharraz <saif.elkharraz@seven-app.net>
2024-11-13 17:38:32 +0100

    feat(dto): add entities dtos

---
+++ chat-back/src/main/java/com/example/chatback/Dtos/ChatRoomDto.java
@@ -0,0 +1,16 @@
+package com.example.chatback.Dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ChatRoomDto {
+    private List<MessageDto> messages;
+}
