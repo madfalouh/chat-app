fe5c8d645e10a2966c86b64889220a644bff6356
s.elkharraz <saif.elkharraz@seven-app.net>
2024-11-15 17:39:33 +0100

    feat: add login and signup functionality without api

--- chat-front/src/app/views/login/login.component.ts
+++ chat-front/src/app/views/login/login.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: '../formStyle.css'
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit {
+  loginForm! : FormGroup;
+  ngOnInit(): void {
+    this.loginForm = new FormGroup(
+      {
+        username : new FormControl(null),
+        password : new FormControl(null)
+      }
+    )
+  }
+  login() {
+    if (this.loginForm) {
+      const { username, password } = this.loginForm.value;
+      console.log("credentials :" , username, password)
+    }
+  }
 }
--- chat-front/src/app/views/signup/signup.component.ts
+++ chat-front/src/app/views/signup/signup.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink , ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: '../formStyle.css'
 })
-export class SignupComponent {
-
+export class SignupComponent implements OnInit {
+  signupForm! : FormGroup;
+  ngOnInit(): void {
+    this.signupForm = new FormGroup(
+      {
+        username : new FormControl(null),
+        password : new FormControl(null),
+        passwordConfirm : new FormControl(null)
+      }
+    )
+  }
+  signup() {
+    if (this.signupForm) {
+      const { username, password , passwordConfirm } = this.signupForm.value;
+      if (password === passwordConfirm) {
+        console.log("credentials :" , username, password)
+      } else {
+        console.log("Passwords should match")
+      }
+    }
+  }
 }
