f587a6e2d0e73017e4ac66e916c974e4e80001e9
madfalouh <reda.falhi10@gmail.com>
2024-11-15 22:59:44 -0600

    mend

--- chat-back/src/main/java/com/example/chatback/Repositories/MessageRepository.java
+++ chat-back/src/main/java/com/example/chatback/Repositories/MessageRepository.java
@@ -3,5 +3,5 @@ package com.example.chatback.Repositories;
 import com.example.chatback.Entities.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MessageService extends JpaRepository<Message, Integer> {
+public interface MessageRepository extends JpaRepository<Message, Integer> {
 }
